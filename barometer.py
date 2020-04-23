76d82c994b23c69bf43182729f012962f0ea90f6
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2020-04-23 09:31:53 +0100

    Include temperature in calculation of altitude

--- barometer.py
+++ barometer.py
@@ -51,92 +51,104 @@ LPS_AMP_CTRL = 0x30
 LPS_SET_ODR = {12.5:0b11100000, # 12.5Hz
                25 : 0b11110000} # 25Hz
 
+########################################################################
+## Conversion function ##
+#########################
+sealevel_pressure = 1013.25 # hPa or mbar
+# See https://keisan.casio.com/has10/SpecExec.cgi?path=06000000.Science%252F02100100.Earth%2520science%252F12000300.Altitude%2520from%2520atmospheric%2520pressure%252Fdefault.xml&charset=utf-8
+def pressure_to_altitude(pressure, temperature):
+    return (((pressure/1013.25)**(1/5.257) - 1) * (temperature + 273.15))/0.0065
 
 ########################################################################
 ## Barometer class ##
 #####################
 class Barometer:
-	def __init__(self, bus, odr=12.5, calibration_num_datapoints=1000):
-		self.bus = bus
-		self.odr = odr
-		self.calibration_num_datapoints = calibration_num_datapoints
-		self.pressure_datum = 0
-		self.altitude_datum = 0
+    def __init__(self, bus, odr=12.5, calibration_num_datapoints=1000):
+        self.bus = bus
+        self.odr = odr
+        self.calibration_num_datapoints = calibration_num_datapoints
+        self.pressure_datum = 0
+        self.altitude_datum = 0
 
-		if self.bus.read_byte_data(LPS, LPS_WHOAMI_ADDRESS) != LPS_WHOAMI_CONTENTS:
-			raise Exception("LPS not found at address {}.".format(LPS))
-		else:
-			# Enable barometer and set ODR 25 Hz
-			self.bus.write_byte_data(LPS, LPS_CTRL_1, LPS_SET_ODR[self.odr])
-			print("Barometer set up.")
+        if self.bus.read_byte_data(LPS, LPS_WHOAMI_ADDRESS) != LPS_WHOAMI_CONTENTS:
+            raise Exception("LPS not found at address {}.".format(LPS))
+        else:
+            # Enable barometer and set ODR 25 Hz
+            self.bus.write_byte_data(LPS, LPS_CTRL_1, LPS_SET_ODR[self.odr])
+            print("Barometer set up.")
 
-	def read(self):
-		# Read pressure from registers
-		pressure_XL = self.bus.read_byte_data(LPS, LPS_PRESS_OUT_XL)
-		pressure_L = self.bus.read_byte_data(LPS, LPS_PRESS_OUT_L)
-		pressure_H = self.bus.read_byte_data(LPS, LPS_PRESS_OUT_H)
-		# Update stored pressure values in mbar see datasheet for formula
-		self.pressure = (pressure_H << 16 | pressure_L << 8 | pressure_XL) / 4096.0
-		self.relative_pressure = self.pressure - self.pressure_datum
-		# Update the stored altitude value
-		self.altitude = 44308.7 * (1 - ((self.pressure/1013.25)**0.190284))
-		self.relative_altitude = self.altitude - self.altitude_datum
-		# Return absolute pressure value
-		return self.pressure
+    def read(self):
+        # Read pressure from registers
+        pressure_XL = self.bus.read_byte_data(LPS, LPS_PRESS_OUT_XL)
+        pressure_L = self.bus.read_byte_data(LPS, LPS_PRESS_OUT_L)
+        pressure_H = self.bus.read_byte_data(LPS, LPS_PRESS_OUT_H)
+        # Read temperature from registers
+        temp_l = self.bus.read_byte_data(LPS, LPS_TEMP_OUT_L)
+        temp_h = self.bus.read_byte_data(LPS, LPS_TEMP_OUT_H)
+        # Update stored pressure values in mbar see datasheet for formula
+        self.pressure = (pressure_H << 16 | pressure_L << 8 | pressure_XL) / 4096.0
+        self.relative_pressure = self.pressure - self.pressure_datum
+        # Update stored temperature values in deg C see datasheet for formula
+        self.temperature = 42.5 + (temp_h << 8 | temp_l) / 480.0
+        # Update the stored altitude value
+        self.altitude = pressure_to_altitude(self.pressure, self.temperature)
+        self.relative_altitude = self.altitude - self.altitude_datum
+        # Return absolute pressure value
+        return self.pressure
 
-	def read_relative_pressure(self):
-		# Update all stored pressure/altitude values
-		self.read()
-		# Return the relative pressure value
-		return self.relative_pressure
+    def read_relative_pressure(self):
+        # Update all stored pressure/altitude values
+        self.read()
+        # Return the relative pressure value
+        return self.relative_pressure
 
-	def read_altitude(self):
-		# Update all stored pressure/altitude values
-		self.read()
-		# Return altitude
-		return self.altitude
+    def read_altitude(self):
+        # Update all stored pressure/altitude values
+        self.read()
+        # Return altitude
+        return self.altitude
+    
+    def read_relative_altitude(self):
+        # Update all stored pressure/altitude values
+        self.read()
+        # Return altitude
+        return self.relative_altitude
 
-	def read_relative_altitude(self):
-		# Update all stored pressure/altitude values
-		self.read()
-		# Return altitude
-		return self.relative_altitude
+    def calibrate(self):
+        pressure_data = np.zeros(self.calibration_num_datapoints)
+        altitude_data = np.zeros(self.calibration_num_datapoints)
 
-	def calibrate(self):
-		pressure_data = np.zeros(self.calibration_num_datapoints)
-		altitude_data = np.zeros(self.calibration_num_datapoints)
-
-		print("Calibrating barometer...")
-		print("Place the sensor on the ground.")
-		for i in np.arange(self.calibration_num_datapoints):
-			pressure_data[i] = self.read()
-			altitude_data[i] = self.read_altitude()
-		# Calculate average value (TODO: replace with a fancy filter)
-		self.pressure_datum = np.mean(pressure_data)
-		self.altitude_datum = np.mean(altitude_data)
-		# Update stored values
-		self.read()
-		print("Calibration successful.")
+        print("Calibrating barometer...")
+        print("Place the sensor on the ground.")
+        for i in np.arange(self.calibration_num_datapoints):
+            pressure_data[i] = self.read()
+            altitude_data[i] = self.read_altitude()
+        # Calculate average value (TODO: replace with a fancy filter)
+        self.pressure_datum = np.mean(pressure_data)
+        self.altitude_datum = np.mean(altitude_data)
+        # Update stored values
+        self.read()
+        print("Calibration successful.")
 
 
 ########################################################################
 ## Main ##
 ##########
 if __name__ == "__main__":
-	from smbus import SMBus
-	from time import sleep
+    from smbus import SMBus
+    from time import sleep
 
-	# Initialise the i2c bus
-	I2CBUS_NUMBER = 1
-	bus = SMBus(I2CBUS_NUMBER)
+    # Initialise the i2c bus
+    I2CBUS_NUMBER = 1
+    bus = SMBus(I2CBUS_NUMBER)
 
-	# Initialise the barometer
-	barometer1 = Barometer(bus)
-	barometer1.calibrate()
+    # Initialise the barometer
+    barometer1 = Barometer(bus)
+    barometer1.calibrate()
 
-	try:
-		while True:
-			print(barometer1.read_relative_altitude())
-			sleep(1/25)
-	except KeyboardInterrupt:
-		print("Exiting...")
+    try:
+        while True:
+            print(barometer1.read_relative_altitude())
+            sleep(1/25)
+    except KeyboardInterrupt:
+        print("Exiting...")
